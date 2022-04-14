43ecb3c256b06c9129b89cc97bf92e06415d90ef
Liz-Conway <zil.conway@gmail.com>
2022-04-14 20:51:24 +0100

    FEAT: Show a basic home page

--- hippotherapy/urls.py
+++ hippotherapy/urls.py
@@ -1,4 +1,4 @@
-"""amber_p4 URL Configuration
+"""amber hippotherapy URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -9,14 +9,17 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('', include('hippotherapy.urls'))
+
 """
-from django.contrib import admin
-from django.urls import path, include
+
+'''
+Created on 14 Apr 2022
+
+@author: liz
+'''
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('hippotherapy.urls'), name='hippo_urls'),
+    path('', views.go_home, name='home'),
 ]
---
+++ hippotherapy/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def go_home(request):
+    return render(request, 'hippo/index.html')
