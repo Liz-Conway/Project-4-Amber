43dc117d7763352e03bed9f5525be83ee066459f
Liz-Conway <zil.conway@gmail.com>
2022-06-13 14:52:33 +0100

    TEST: Add first test for POST - Add Client page

--- hippotherapy/test/test_views.py
+++ hippotherapy/test/test_views.py
@@ -3,19 +3,18 @@ Created on 14 May 2022
 
 @author: liz
 '''
-from django.test import TestCase, Client, client
+from django.test import TestCase, Client as TestClient
 from hippotherapy.models import Session, Skill, SkillScore, Course, Hat,\
     Function
-from hippotherapy.models import Client as HipClient
-from datetime import date
+from hippotherapy.models import Client
 from administration.models import Diagnosis, Horse
 from django.urls.base import reverse
-from _datetime import datetime
+from datetime import datetime
 
 class ViewsTest(TestCase):
     def setUp(self):
-        self.hat = Hat.objects.create(size='4')
-        self.hip_client = HipClient.objects.create(
+        self.hat = Hat.objects.create(size='5')
+        self.hip_client = Client.objects.create(
             first_name = 'Mister',
             last_name = 'Blobby',
             gender = 'M',
@@ -36,6 +35,10 @@ class ViewsTest(TestCase):
             horse = self.horse
         )
         
+        self.hat_sizes = ['1']  # Refers to the ID of the hat in the hat table
+        self.genders = ['M', 'F']
+        self.diagnosis1 = Diagnosis.objects.create(diagnosis='ASD')
+        
         # Create some skills
         self.function = Function.objects.create(function_name='Test Function')
         self.basket_weaving = Skill.objects.create(skill_name="Basket Weaving", function=self.function)
@@ -47,7 +50,7 @@ class ViewsTest(TestCase):
         self.flower_score = SkillScore.objects.create(session=self.hip_session, skill=self.flower_arranging, score=3)
         self.sewing_score = SkillScore.objects.create(session=self.hip_session, skill=self.embroidery, score=5)
 
-        self.client = Client()
+        self.test_client = TestClient()
         self.home_url = reverse('home')
         self.add_client_url = reverse('addClient')
         self.record_session_url = reverse('recordSession', args=[self.hip_client.id])
@@ -55,98 +58,119 @@ class ViewsTest(TestCase):
         self.observe_session_url = reverse('observeSession', args=[self.hip_session.id])
         self.generate_chart_url = reverse('generateChart', args=[self.course.id])
         self.choose_course_client = reverse('chooseCourse', args=[self.hip_client.id])
-        self.choose_course_client_session = reverse('chooseCourse', args=[self.hip_client.id, self.hip_session.id])
         self.new_course = reverse('newCourse', args=[self.hip_client.id, self.hip_session.id])
         
     def test_show_home_page(self):
-        response = self.client.get(self.home_url)
+        response = self.test_client.get(self.home_url)
         
         self.assertEqual(response.status_code, 200, "Going to home page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/index.html", "Home page should use the index.html template")
 
     def test_show_home_page_blank_url(self):
-        response = self.client.get('')
+        response = self.test_client.get('')
         
         self.assertEqual(response.status_code, 200, "Going to home page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/index.html", "Home page should use the index.html template")
 
     def test_get_add_client(self):
-        response = self.client.get(self.add_client_url)
+        response = self.test_client.get(self.add_client_url)
         
         self.assertEqual(response.status_code, 200, "Going to Add Client page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/addClient.html", "Add Client page should use the addClient.html template")
 
+    def test_post_add_client(self):
+        diagnosis1_label = f'diagnosis_{self.diagnosis1.id}'
+        post_data = {
+            'first_name': 'Donald',
+            'last_name': 'Duck',
+            'gender': self.genders[0],
+            'date_of_birth': '09/06/1931',
+            'hat_size': self.hat.id,
+            diagnosis1_label: 'on',
+            'degree_of_difficulty': 'Gets annoyed very quickly',
+            'additional_notes': 'Speaks quackily'
+        }
+        
+        response = self.test_client.post(self.add_client_url, post_data)
+        messages = list(response.context['messages'])
+        
+        # self.assertEqual(response.status_code, 302, 'Posting the Add Client form should cause a redirect (302)')
+        # Since we redisplay the Add Client page again after successfully adding a client
+        # the Status Code is 200 because we used a GET instead of a redirect
+        self.assertEqual(response.status_code, 200, 'Posting the Add Client form should return a Status Code of 200 (OK)')
+        
+        self.assertEqual(Client.objects.last().first_name, 'Donald', 'First name should be Donald')
+        self.assertEqual(Client.objects.last().last_name, 'Duck', 'Last name should be Duck')
+        self.assertEqual(Client.objects.last().gender, 'M', 'Gender should be Male')
+        self.assertEqual(Client.objects.last().date_of_birth, datetime.strptime('09/06/1931', '%d/%m/%Y').date(), 'Donald Duck was born on 9th June 1931')
+        self.assertEqual(Client.objects.last().hat_size, self.hat, 'Donald Duck has a hat size of 5')
+        # For many-to-many relationships - Need to use 'get' to retrieve the associated object
+        self.assertEqual(Client.objects.last().diagnosis.get(), self.diagnosis1, 'Donald Duck has the first diagnosis on the list')
+        self.assertEqual(Client.objects.last().degree_of_difficulty, 'Gets annoyed very quickly', 'Donald certainly Gets annoyed very quickly')
+        self.assertEqual(Client.objects.last().additional_notes, 'Speaks quackily', 'Additional notes should say :  Speaks quackily')
+        
+        self.assertTemplateUsed(response, "addClient.html", "Add Client page should use the addClient.html template")
+        
     def test_get_record_session(self):
-        response = self.client.get(self.record_session_url)
+        response = self.test_client.get(self.record_session_url)
         
         self.assertEqual(response.status_code, 200, "Going to Record Session page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/recordSession.html", "Record Session page should use the recordSession.html template")
 
     def test_get_record_session_no_client(self):
-        response = self.client.get(reverse('recordSession', args=[666]))
+        response = self.test_client.get(reverse('recordSession', args=[666]))
         
         self.assertEqual(response.status_code, 404, "Going to Record Session page with a non-existant client should return a Status Code of 404")
 
     def test_get_select_client(self):
-        response = self.client.get(self.select_client_url)
+        response = self.test_client.get(self.select_client_url)
         
         self.assertEqual(response.status_code, 200, "Going to Select Client page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/selectClient.html", "Select Client page should use the selectClient.html template")
 
     def test_get_select_client_any_target(self):
-        response = self.client.get(reverse('selectClient', args=['any target']))
+        response = self.test_client.get(reverse('selectClient', args=['any target']))
         
         self.assertEqual(response.status_code, 200, "Going to Select Client page should return a Status Code of 200 (OK) regardless of the target passed in")
         self.assertTemplateUsed(response, "hippo/selectClient.html", "Select Client page should use the selectClient.html template")
 
     def test_get_observe_session(self):
-        response = self.client.get(self.observe_session_url)
+        response = self.test_client.get(self.observe_session_url)
     
         self.assertEqual(response.status_code, 200, "Going to Observe Session page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/observeSession.html", "Observe Session page should use the observeSession.html template")
 
     def test_get_observe_session_no_session(self):
-        response = self.client.get(reverse('observeSession', args=[666]))
+        response = self.test_client.get(reverse('observeSession', args=[666]))
     
         self.assertEqual(response.status_code, 404, "Going to Observe Session page with a non-existant session should return a Status Code of 404")
 
     def test_get_generate_chart(self):
         # Needed to add some skill scores for this to work
-        response = self.client.get(self.generate_chart_url)
+        response = self.test_client.get(self.generate_chart_url)
     
         self.assertEqual(response.status_code, 200, "Going to Generate Chart page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/generateChart.html", "Generate Chart page should use the generateChart.html template")
 
     def test_get_generate_chart_no_course(self):
         # Needed to add some skill scores for this to work
-        response = self.client.get(reverse('generateChart', args=[666]))
+        response = self.test_client.get(reverse('generateChart', args=[666]))
     
         self.assertEqual(response.status_code, 404, "Going to Generate Chart page with a non-existant course should return a Status Code of 404")
 
     def test_get_choose_course_client_only(self):
-        response = self.client.get(self.choose_course_client)
+        response = self.test_client.get(self.choose_course_client)
     
         self.assertEqual(response.status_code, 200, "Going to Choose Course page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/chooseCourse.html", "Choose Course page should use the chooseCourse.html template")
 
     def test_get_choose_course_no_client(self):
-        response = self.client.get(reverse('chooseCourse', args=[666]))
+        response = self.test_client.get(reverse('chooseCourse', args=[666]))
     
         self.assertEqual(response.status_code, 404, "Going to Choose Course page with a non-existant client should return a Status Code of 404")
 
-    def test_get_choose_course_client_session(self):
-        response = self.client.get(self.choose_course_client_session)
-    
-        self.assertEqual(response.status_code, 200, "Going to Choose Course page should return a Status Code of 200 (OK)")
-        self.assertTemplateUsed(response, "hippo/chooseCourse.html", "Choose Course page should use the chooseCourse.html template")
-
-    def test_get_choose_course_no_session(self):
-        response = self.client.get(reverse('chooseCourse', args=[self.hip_client.id, 666]))
-    
-        self.assertEqual(response.status_code, 404, "Going to Choose Course page with a non-existant session should return a Status Code of 404")
-
     def test_get_new_course(self):
-        response = self.client.get(self.new_course)
+        response = self.test_client.get(self.new_course)
     
         self.assertEqual(response.status_code, 200, "Going to New Course page should return a Status Code of 200 (OK)")
         self.assertTemplateUsed(response, "hippo/newCourse.html", "New Course page should use the newCourse.html template")
